7c72baa56ede99032af063768b5be2f66cc81e1b
griddles <89864073+griddles@users.noreply.github.com>
2021-12-15 10:14:56 -0700

    started setting up server.py

---
+++ server.py
@@ -0,0 +1,15 @@
+import socket
+import _thread
+import sys
+
+server = ""
+port = 6969
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen()
