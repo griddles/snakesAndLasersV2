403e71995d9f1da4d8820033c465fde5da5c9edb
griddles <89864073+griddles@users.noreply.github.com>
2021-12-15 10:31:54 -0700

    completed basic server setup

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-import _thread
+from _thread import *
 import sys
 
 server = ""
@@ -12,4 +12,28 @@ try:
 except socket.error as e:
     str(e)
 
-s.listen()
+s.listen(2)
+print("Server online, waiting for client connections...")
+
+def threaded_client(conn):
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("Client disconnected")
+                break
+            else:
+                print("Recieved {}".format(reply))
+            conn.sendall(str.encode(reply))
+        except:
+            print("Data reception error")
+            break
+
+while True:
+    conn, addr = s.accept()
+    print("Connected to {}".format(addr))
+
+    start_new_thread(threaded_client, (conn))
