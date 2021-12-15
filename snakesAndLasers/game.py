1865ce371526bcdc4d0dd21497eab332a1e2347a
griddles <89864073+griddles@users.noreply.github.com>
2021-12-15 09:38:22 -0700

    hopefully this fixes the problem

---
+++ snakesAndLasers/game.py
@@ -0,0 +1,38 @@
+import pygame as pg
+import tkinter as tk
+
+tk = tk.Tk()
+screenWidth = tk.winfo_screenwidth()
+screenHeight = tk.winfo_screenheight()
+
+class Segment:
+    def __init__(self, posX, posY, dir):
+        self.direction = dir
+        self.rect = pg.Rect((posX, posY), (25, 25))
+
+class TurnPos:
+    def __init__(self, xPos, yPos, dir, ticks):
+        self.x = xPos
+        self.y = yPos
+        self.direction = dir
+        self.time = ticks
+
+class Laser:
+    def __init__(self, dir, position, startTime, moveDir):
+        self.direction = dir
+        self.pos = position
+        self.time = startTime
+        self.moveDirection = moveDir
+        if dir == "V":
+            self.rect = pg.Rect((position, 0), (25, screenHeight))
+        else:
+            self.rect = pg.Rect((0, position), (screenWidth, 25))
+    width = 5
+
+class Particle:
+    def __init__(self, posX, posY, speedX, speedY, ticks):
+        self.x = posX
+        self.y = posY
+        self.xSpeed = speedX
+        self.ySpeed = speedY
+        self.time = ticks
