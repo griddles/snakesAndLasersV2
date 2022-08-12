fca0d48966ef60dd605873aae28f5433a88329d1
griddles <89864073+griddles@users.noreply.github.com>
2022-08-12 13:29:57 -0700

    removed comments

--- client.py
+++ client.py
@@ -1,31 +1,3 @@
-"""
-Snakes and Lasers v2.1.1 Client
-A lightweight 2d arcade style game based on the classic Snake game, but with a non-osha compliant twist.
-
-How to play:
- - WASD to move the snake
- - R to return to main menu
- - Speedrun mode activates a timer and automatically ends the game once you reach 200 points.
- - Lightweight mode removes all laser particles and screenshake to reduce lag on very low end systems 
-   (like the laptop I used to make this game).
- - Survival mode removes the objective and decreases the time between lasers.   
- - All three of these modes work at the same time, though Survival mode kinda defeats the purpose of 
-   Speedrun mode since the only possible time is 3:20.000. Also, it's nearly impossible to survive for 
-   that long.
- - Multiplayer mode connects you to the server at the ip address in the code. If there isn't one running 
-   at that IP, it crashes. The mode allows you and one other player to share the screen, collecting the 
-   same objective, sharing a score, and dodging the same lasers. Lightweight mode is built in, since 
-   particles are completely broken, Survive mode does nothing, and Speedrun mode disconnects you if the 
-   score is higher than 200. If the server score is already higher, it prevents you from connecting.
-
-Credit:
-Code - Me
-Sprites - Me
-Sound Effects - Me
-Music - Me
-Playtesting - Me, VoldmortII, Theyes, and NinjaPerfect
-"""
-
 import pygame as pg
 import random as rnd
 import tkinter as tk
